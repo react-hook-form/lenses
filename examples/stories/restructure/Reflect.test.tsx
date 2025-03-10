c3e7e04ad21c829abca4d95f9313c04677e48b96
aspirisen <aspirisen@gmail.com>
2025-03-10 17:23:41 +0300

    Add array reflect tests

--- examples/stories/restructure/Reflect.test.tsx
+++ examples/stories/restructure/Reflect.test.tsx
@@ -4,7 +4,7 @@ import user from '@testing-library/user-event';
 
 import * as stories from './Reflect.story';
 
-const { Reflect } = composeStories(stories);
+const { Reflect, ArrayReflect } = composeStories(stories);
 
 test('should change lens via reflect', async () => {
   const handleSubmit = vi.fn();
@@ -19,7 +19,35 @@ test('should change lens via reflect', async () => {
     {
       firstName: 'joe',
       lastName: { value: 'doe' },
-    },
+    } satisfies stories.ReflectFormData,
+    expect.anything(),
+  );
+});
+
+test('should change array lens via reflect', async () => {
+  const handleSubmit = vi.fn();
+  render(<ArrayReflect onSubmit={handleSubmit} />);
+
+  expect(screen.getByPlaceholderText('items.0.value.inside')).toHaveValue('one');
+  expect(screen.getByPlaceholderText('items.1.value.inside')).toHaveValue('two');
+  expect(screen.getByPlaceholderText('items.2.value.inside')).toHaveValue('three');
+
+  await user.click(screen.getByText(/Add item/i));
+
+  await user.type(screen.getByPlaceholderText('items.3.value.inside'), ' four');
+  expect(screen.getByPlaceholderText('items.3.value.inside')).toHaveValue('more four');
+
+  await user.click(screen.getByText(/submit/i));
+
+  expect(handleSubmit).toHaveBeenCalledWith(
+    {
+      items: [
+        { value: { inside: 'one' } },
+        { value: { inside: 'two' } },
+        { value: { inside: 'three' } },
+        { value: { inside: 'more four' } },
+      ],
+    } satisfies stories.ArrayReflectFormData,
     expect.anything(),
   );
 });
--- examples/tests/reflect.test.ts
+++ examples/tests/reflect.test.ts
@@ -1,5 +1,6 @@
 import { useForm } from 'react-hook-form';
 import { Lens, useLens } from '@hookform/lenses';
+import { useFieldArray } from '@hookform/lenses/rhf';
 import { renderHook } from '@testing-library/react';
 import { expectTypeOf } from 'vitest';
 
@@ -61,4 +62,33 @@ test('reflect can add props from another lens', () => {
   expectTypeOf(form1.current.reflect((l) => ({ c: l.focus('a'), d: form2.current.focus('b') }))).toEqualTypeOf<
     Lens<{ c: string; d: number }>
   >();
+});
+
+test('reflect can work with array', () => {
+  const { result } = renderHook(() => {
+    const form = useForm<{ items: { value: string }[] }>({ defaultValues: { items: [{ value: 'one' }, { value: 'two' }] } });
+
+    const lens = useLens({ control: form.control })
+      .focus('items')
+      .reflect((l) => [{ another: l.focus('value') }]);
+
+    const arr = useFieldArray(lens.interop());
+
+    return { form, lens, arr };
+  });
+
+  expectTypeOf(result.current.lens).toEqualTypeOf<Lens<{ another: string }[]>>();
+
+  const [one, two] = result.current.lens.map(result.current.arr.fields, (l) => l.focus('another').interop());
+
+  expect(one).toEqual({
+    name: 'items.0.value',
+    control: result.current.form.control,
+    lens: result.current.lens.focus('0').focus('another'),
+  });
+  expect(two).toEqual({
+    name: 'items.1.value',
+    control: result.current.form.control,
+    lens: result.current.lens.focus('1').focus('another'),
+  });
 });
