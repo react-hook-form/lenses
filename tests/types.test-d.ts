119e7559a13a36fe6652f42fc485d608ee1dfa7d
Dmitrii Pikulin <aspirisen@gmail.com>
2025-08-02 14:19:00 +0300

    Use transformed type from Controller for Lens creation (#34)

--- tests/types.test-d.ts
+++ tests/types.test-d.ts
@@ -1,5 +1,28 @@
-import type { Lens, UnwrapLens } from '@hookform/lenses';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useForm } from 'react-hook-form';
+import { type Lens, type UnwrapLens, useLens } from '@hookform/lenses';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { expectTypeOf } from 'vitest';
+import { z } from 'zod';
+
+test('lens should use output type of the Controller', () => {
+  const schema = z.object({
+    id: z.number().transform((val) => val.toString()),
+  });
+
+  type Input = z.input<typeof schema>;
+  type Output = z.output<typeof schema>;
+
+  expectTypeOf<Input>().toEqualTypeOf<{ id: number }>();
+  expectTypeOf<Output>().toEqualTypeOf<{ id: string }>();
+
+  const form = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const lens = useLens({ control: form.control });
+  expectTypeOf<typeof lens>().toEqualTypeOf<Lens<Output>>();
+});
 
 test('lens unwraps to the original type', () => {
   expectTypeOf<UnwrapLens<Lens<{ a: string }>>>().toEqualTypeOf<{ a: string }>();
