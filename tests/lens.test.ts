97b8eb9eeccd91659399420d4ef4a03462b9eeb9
Dmitrii Pikulin <aspirisen@gmail.com>
2025-08-02 15:03:01 +0300

    Make root lens to be watchable when name is an empty string (#27) (#30)

--- tests/lens.test.ts
+++ tests/lens.test.ts
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form';
+import { useForm, useWatch } from 'react-hook-form';
 import { type Lens, useLens } from '@hookform/lenses';
 import { renderHook } from '@testing-library/react';
 import { expectTypeOf } from 'vitest';
@@ -11,7 +11,7 @@ test('lens can be created', () => {
   });
 
   expectTypeOf(result.current.lens).toEqualTypeOf<Lens<{ a: string }>>();
-  expect(result.current.lens.interop()).toEqual({ name: '', control: result.current.form.control });
+  expect(result.current.lens.interop()).toEqual({ name: undefined, control: result.current.form.control });
 });
 
 test('lenses are different when created with different forms', () => {
@@ -26,4 +26,26 @@ test('lenses are different when created with different forms', () => {
   expect(result.current.lens1.interop()).toEqual(result.current.lens2.interop());
   expect(result.current.form.control).toBe(result.current.lens1.interop().control);
   expect(result.current.form.control).toBe(result.current.lens2.interop().control);
+});
+
+test('root lens should be watchable when name is an empty string in LensCore', () => {
+  const { result } = renderHook(() => {
+    const form = useForm({
+      defaultValues: {
+        a: 3,
+        b: 'hello',
+      },
+    });
+
+    const lens = useLens({ control: form.control });
+    const interop = lens.interop();
+    const value = useWatch(interop);
+
+    return { form, interop, value };
+  });
+
+  expect(result.current.interop).toEqual({ name: undefined, control: result.current.form.control });
+  expect(result.current.value).toEqual({ a: 3, b: 'hello' });
+  expect(result.current.value.a).toEqual(3);
+  expect(result.current.value.b).toEqual('hello');
 });
